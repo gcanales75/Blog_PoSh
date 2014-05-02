f70e89f7034a537bb92d25757f34e4338ddbbc1d
gcanales75 <gcanales75@gmail.com>
2014-05-02 12:15:17 -0500

    ESXi-syslog-Splunk
    Configurar syslog y exportar a Splunk Storm

---
+++ config-syslog-splunk.ps1
@@ -0,0 +1,3 @@
+﻿Get-VMHost $interpol | Set-VMHostAdvancedConfiguration -Name Syslog.global.logHost -Value udp.7972-u5ax.data.splunkstorm.com
+Get-VMHost $interpol | Set-VMHostSysLogServer -SysLogServer udp.7972-u5ax.data.splunkstorm.com -SysLogServerPort 41212
+Get-VMHost $interpol | Get-VMHostFirewallException | where {$_.Name.StartsWith('syslog-Splunk')} | Set-VMHostFirewallException -Enabled $true
